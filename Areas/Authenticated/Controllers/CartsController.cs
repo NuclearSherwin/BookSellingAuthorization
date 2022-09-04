dd05c3ead585d17962d7f0f0cfe36f3ccc885204
NuclearSherwin <Phongspacenasa@gmail.com>
2022-09-04 14:35:18 +0700

    add sumary

--- Areas/Authenticated/Controllers/CartsController.cs
+++ Areas/Authenticated/Controllers/CartsController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using bookselling.Data;
+using bookselling.Models;
 using bookselling.Utils;
 using bookselling.ViewModels;
 using Microsoft.AspNetCore.Authorization;
@@ -115,10 +116,90 @@ namespace bookselling.Controllers
         }
 
         // summary
-        // [HttpGet]
-        // public IActionResult Summary()
-        // {
-        //     
-        // }
+        [HttpGet]
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            ShoppingCartVm = new ShoppingCartVM()
+            {
+                OrderHeader = new Models.OrderHeader(),
+                ListCarts = _db.Carts.Where(u => u.CustomerId == claim.Value).Include(c => c.Book)
+            };
+
+            ShoppingCartVm.OrderHeader.ApplicationUser = _db.ApplicationUsers
+                .FirstOrDefault(u => u.Id == claim.Value);
+
+            foreach (var list in ShoppingCartVm.ListCarts)
+            {
+                list.Price = list.Book.Price;
+                ShoppingCartVm.OrderHeader.Total += (list.Price + list.Count);
+            }
+
+            ShoppingCartVm.OrderHeader.Address = ShoppingCartVm.OrderHeader.ApplicationUser.Address;
+            ShoppingCartVm.OrderHeader.OrderDate = DateTime.Now;
+
+            return View(ShoppingCartVm);
+        }
+        
+        // summary post
+        [HttpPost]
+        [ActionName("Summary")]
+        [ValidateAntiForgeryToken]
+        public IActionResult SummaryPost()
+        {
+            // lay id cua user
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            // lay thong tin user dang mua
+            // lay toan bo list cart
+            ShoppingCartVm.OrderHeader.ApplicationUser = _db.ApplicationUsers
+                .FirstOrDefault(c => c.Id == claim.Value);
+            ShoppingCartVm.ListCarts = _db.Carts.Where(c => c.CustomerId == claim.Value)
+                .Include(c => c.Book);
+
+            
+            // assign value for each field in order header
+            ShoppingCartVm.OrderHeader.CustomerId = claim.Value;
+            ShoppingCartVm.OrderHeader.Address = ShoppingCartVm.OrderHeader.ApplicationUser.Address;
+            ShoppingCartVm.OrderHeader.OrderDate = DateTime.Now;
+
+            // add to order header and save changes to get order header id
+            _db.OrderHeaders.Add(ShoppingCartVm.OrderHeader);
+            _db.SaveChanges();
+
+            // moi san pham add vao order detail
+            foreach (var item in ShoppingCartVm.ListCarts)
+            {
+                item.Price = item.Book.Price;
+                OrderDetail orderDetail = new OrderDetail()
+                {
+                    BookId = item.BookId,
+                    OrderHeaderId = ShoppingCartVm.OrderHeader.Id,
+                    Price = item.Price,
+                    Quantity = item.Count
+                };
+
+                // calculate total for order header and add to order detail
+                ShoppingCartVm.OrderHeader.Total += orderDetail.Quantity + orderDetail.Price;
+                _db.OrderDetails.Add(orderDetail);
+            }
+            
+            // remove that item from cart
+            _db.Carts.RemoveRange(ShoppingCartVm.ListCarts);
+            _db.SaveChanges();
+            HttpContext.Session.SetInt32(SD.ssShoppingCart, 0);
+
+            return RedirectToAction("OrderConfirmation", "Carts", 
+                new { id = ShoppingCartVm.OrderHeader.Id });
+        }
+        
+        // order confirm 
+        public IActionResult OrderConfirmation(int id)
+        {
+            return View(id);
+        }
     }
 }
